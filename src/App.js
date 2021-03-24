aa9335fc838a45170786af1ca57cc417be4e8a58
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2021-03-24 14:47:24 +0100

    still under setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
+
+
 function App() {
   return (
-    
+    <Suspense fallback={}>
+      <Switch>
+        
+      </Switch>
+    </Suspense>
   );
 }
 
