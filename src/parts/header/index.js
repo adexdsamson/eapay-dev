a29f40316d30ce37e045d4eba444d6d32f845f3e
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2021-03-28 19:09:28 +0100

    Layout, sidebar, header responsiveness

--- src/parts/header/index.js
+++ src/parts/header/index.js
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 import { Avatar } from "@material-ui/core";
 import { Bell, BellOutline, Menu } from "heroicons-react";
 
-const Header = ({ title, src, children, isMobile, isNotification, onMenu }) => {
+const Header = ({ title, src, mode, isMobile, isNotification, onMenu }) => {
   return (
     <div className="page-header d-flex align-items-center justify-content-between">
       <div className="d-flex align-items-center">
         {isMobile ? <Menu size={35} onClick={onMenu} /> : null}
-        <h4 className="mb-0 ml-2">Dashboard</h4>
+        <h4 className="mb-0 ml-2">{title}</h4>
       </div>
       <div className="pr-4 d-flex align-items-center">
         {isMobile ? null : (
-          <p className="ml-3 mr-3 mb-0 text-danger">Test mode</p>
+          <p className="ml-3 mr-3 mb-0 text-danger">{mode}</p>
         )}
         {isNotification ? (
           <Bell className="ml-3 mr-3 mb-0 text-eapay" />
@@ -25,6 +25,13 @@ const Header = ({ title, src, children, isMobile, isNotification, onMenu }) => {
   );
 };
 
-Header.propTypes = {};
+Header.propTypes = {
+  title: PropTypes.string, 
+  src: PropTypes.string, 
+  mode: PropTypes.string, 
+  isMobile: PropTypes.bool, 
+  isNotification: PropTypes.bool, 
+  onMenu: PropTypes.func
+};
 
 export default Header;
