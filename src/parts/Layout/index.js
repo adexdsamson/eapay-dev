91acc0fb736d6bc86ebe0b90f5b4dd7cf88cdaf8
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2021-03-28 18:51:48 +0100

    Responsive sidebar and header
    made the sidebar responsive and made it to show when clicked

--- src/parts/Layout/index.js
+++ src/parts/Layout/index.js
@@ -1,19 +1,31 @@
-// import {} from 'react';
+import { useState } from "react";
 import "./index.css";
 import Sidebar from "../sidebar";
 import Header from "../header";
-import UseMediaQuery from '../'
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { Fade } from "react-reveal";
 
 const Layout = ({ children }) => {
+  const isMobile = useMediaQuery("down", "md");
+  const [drawer, setDrawer] = useState(false);
+  const openDrawer = drawer ? "d-block" : "d-none";
   return (
     <div className="page-wrapper">
       <div className="page-body-wrapper">
-        <div className="page-sidebar">
-          <Sidebar />
+        <div
+          onClick={() => setDrawer(false)}
+          className={`page-sidebar ${isMobile ? openDrawer : "d-block"}`}
+        >
+          {isMobile ? null : <Sidebar />}
+          {isMobile ? (
+            <Fade when={drawer} left>
+              <Sidebar />
+            </Fade>
+          ) : null}
         </div>
         <div className="page-body">
           <div className="container">
-            <Header />
+            <Header onMenu={() => setDrawer(true)} isMobile={isMobile} />
             {children}
           </div>
         </div>
--- src/parts/header/index.js
+++ src/parts/header/index.js
@@ -1,23 +1,30 @@
-import './index.css'
-import PropTypes from 'prop-types';
-import { Avatar } from '@material-ui/core'
+import "./index.css";
+import PropTypes from "prop-types";
+import { Avatar } from "@material-ui/core";
+import { Bell, BellOutline, Menu } from "heroicons-react";
 
-
-const Header = ({ title, src, children, isMobile }) => {
-  return ( 
-    <div className='page-header d-flex align-items-center justify-content-between'>
-      <h3 className='mb-0'>Dashboard</h3>
-      <div className='pr-4 d-flex align-items-center'>
-        {isMobile ? null :<p className='ml-3 mr-3 mb-0 text-danger'>Test mode</p>}
-        <p className='ml-3 mr-3 mb-0'>hello</p>
+const Header = ({ title, src, children, isMobile, isNotification, onMenu }) => {
+  return (
+    <div className="page-header d-flex align-items-center justify-content-between">
+      <div className="d-flex align-items-center">
+        {isMobile ? <Menu size={35} onClick={onMenu} /> : null}
+        <h4 className="mb-0 ml-2">Dashboard</h4>
+      </div>
+      <div className="pr-4 d-flex align-items-center">
+        {isMobile ? null : (
+          <p className="ml-3 mr-3 mb-0 text-danger">Test mode</p>
+        )}
+        {isNotification ? (
+          <Bell className="ml-3 mr-3 mb-0 text-eapay" />
+        ) : (
+          <BellOutline  className="ml-3 mr-3 mb-0 text-muted" />
+        )}
         <Avatar src={src} />
       </div>
     </div>
-   );
-}
+  );
+};
 
-Header.propTypes = {
-  
-}
- 
+Header.propTypes = {};
+
 export default Header;
